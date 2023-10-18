7f66936478924c3c5db5580d25f7c4f108a08e29
VladGelen <vladislavsirotin2004@gmail.com>
2023-10-18 20:11:17 +0300

    -

--- Exercise6/script/script.js
+++ Exercise6/script/script.js
@@ -1,35 +1,7 @@
-function updateOptions() {
-    const serviceType = document.querySelector('input[name="serviceType"]:checked').value;
-    const optionGroup = document.getElementById('optionGroup');
-    const propertyGroup = document.getElementById('propertyGroup');
+function calculateTotal() {
+    const productPrice = parseInt(document.getElementById("product").value);
+    const quantity = parseInt(document.getElementById("quantity").value);
+    const totalPrice = productPrice * quantity;
 
-    if (serviceType === '1') {
-      optionGroup.style.display = 'none';
-      propertyGroup.style.display = 'none';
-    } else if (serviceType === '2') {
-      optionGroup.style.display = 'block';
-      propertyGroup.style.display = 'none';
-    } else if (serviceType === '3') {
-      optionGroup.style.display = 'none';
-      propertyGroup.style.display = 'block';
-    }
-  }
-
-  function calculateTotal() {
-    const quantity = parseInt(document.getElementById('quantity').value);
-    const serviceType = document.querySelector('input[name="serviceType"]:checked').value;
-    let totalPrice = 0;
-
-    if (serviceType === '1') {
-      totalPrice = 10 * quantity;  // Пример цены для типа 1
-    } else if (serviceType === '2') {
-      const optionPrice = parseFloat(document.getElementById('option').value);
-      totalPrice = (10 + optionPrice) * quantity;  // Пример цены для типа 2
-    } else if (serviceType === '3') {
-      const propertyChecked = document.getElementById('property').checked;
-      const propertyPrice = propertyChecked ? 3 : 0;
-      totalPrice = (10 + propertyPrice) * quantity;  // Пример цены для типа 3
-    }
-
-    document.getElementById('totalPrice').innerHTML = `Общая стоимость заказа: ${totalPrice} у.е.`;
+    document.getElementById("totalPrice").innerHTML = "Общая стоимость заказа: $" + totalPrice;
   }

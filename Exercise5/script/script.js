052570f50c338535e75d758b5d306c991f8925a4
VladGelen <vladislavsirotin2004@gmail.com>
2023-10-21 17:46:16 +0300

    0

--- Exercise5/script/script.js
+++ Exercise5/script/script.js
@@ -1,7 +1,22 @@
-function calculateTotal() {
+window.addEventListener('DOMContentLoaded', function (event) {
+  console.log("DOM fully loaded and parsed");
+
+  let b = document.getElementById("my-button");
+  b.addEventListener("click", function (event) {
+    event.preventDefault();
+    alert("Рассчитываем стоимость");
+    calculateTotal();
+  });
+
+  function calculateTotal() {
     const productPrice = parseInt(document.getElementById("product").value);
     const quantity = parseInt(document.getElementById("quantity").value);
+    if (quantity < 0) {
+      alert("Количество не может быть отрицательным.");
+      return;
+    }
     const totalPrice = productPrice * quantity;
 
     document.getElementById("totalPrice").innerHTML = "Общая стоимость заказа: $" + totalPrice;
   }
+});
